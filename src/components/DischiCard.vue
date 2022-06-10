992922fdbaa5f6fd923ca14a9b63d53eadd61b73
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-06-10 16:15:46 +0200

    push

---
+++ src/components/DischiCard.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="disc">
+    <img :src="discobject.poster" :alt="discobject.title">
+</div>
+</template>
+
+<script>
+export default {
+    name: 'DischiCard',
+    props: {
+        discobject: Object
+    }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/DischiList.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    <DischiCard v-for="(item, index) in castDisc" :key="index"
+     :discobject= "item"
+     />
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+import DischiCard from './DischiCard.vue';
+
+export default {
+    name: 'DischiList',
+    components: {
+    DischiCard
+},
+    data(){
+        return{
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
+            castDisc: []
+        }
+    },
+    created(){
+        this.getDisc();
+    },
+    methods: {
+        getDisc(){
+            axios.get(this.apiUrl)
+            .then(result =>{
+            this.castDisc = result.data
+            console.log(result);
+        })
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
